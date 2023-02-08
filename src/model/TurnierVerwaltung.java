76a1c21a8565100aebe18888135637d6519b1813
Hinda <hind.aarif@gmail.com>
2023-02-08 18:01:16 +0100

    Up1 model

--- src/model/TurnierVerwaltung.java
+++ src/model/TurnierVerwaltung.java
@@ -1,14 +1,23 @@
 package model;
 
+import java.util.Date;
+
 import controller.Controller;
 
-/***
- * Das Model dieser MVC-Anwendung
- * -> Aufgaben :
+/**	
+ * 	Das Model dieser MVC-Anwendung
  * 
- * -DB-Verbindung, Daten laden /speichern
- * -Vermittlung von Statusmeldungen und Daten an den Controller/View
- * -Prüfungen. Eingabe-Validierungen
+ * 	-> Aufgaben: 
+ * 
+ * 		- DB-Verbindung, Daten laden/speichern
+ * 		- Vermittlung von Statusmeldungen und Daten an den Controller/View
+ * 		- Prüfungen, Eingabe-Validierungen
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  *
  */
 public class TurnierVerwaltung {
@@ -17,6 +26,34 @@ public class TurnierVerwaltung {
 	public TurnierVerwaltung(Controller controller) {
 		this.controller=controller;
 	}
-
+	
+	//mit der DB verbinden
+	public void dbConnect() {
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	//neues turnier in db speichern
+	public void turnierSpeichern(String turnierName, String ort, String beginn, String ende, String status) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
